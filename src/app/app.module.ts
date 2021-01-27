b540857dca57993658de6f3203779b405a64383f
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-01-27 16:20:23 +0200

    Half work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,12 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { VgCoreModule } from "@videogular/ngx-videogular/core";
-import { VgControlsModule } from "@videogular/ngx-videogular/controls";
-import { VgOverlayPlayModule } from "@videogular/ngx-videogular/overlay-play";
-import { VgBufferingModule } from "@videogular/ngx-videogular/buffering";
-import { VgStreamingModule } from "@videogular/ngx-videogular/streaming";
-
 import { AppComponent } from "./app.component";
 import { CameraComponent } from "./camera/camera.component";
 
 @NgModule({
   declarations: [AppComponent, CameraComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-    VgStreamingModule,
-  ],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { VgApiService, BitrateOptions } from "@videogular/ngx-videogular/core";
+import { VgHlsDirective } from "@videogular/ngx-videogular/streaming";
 
+export interface IMediaStream {
+  type: "hls";
+  source: string;
+  label: string;
+}
 declare var Hls;
 
 @Component({
@@ -8,12 +15,38 @@ declare var Hls;
   styleUrls: ["./camera.component.css"],
 })
 export class CameraComponent implements OnInit {
+  @ViewChild(VgHlsDirective, { static: true }) vgHls: VgHlsDirective[] = [];
+
+  currentStream: IMediaStream;
+  api: VgApiService[];
+
+  bitrates: BitrateOptions[] = [];
   model = {
     object: true,
     segmentation: true,
     rowImage: true,
   };
-  constructor() {}
 
-  ngOnInit(): void {}
+  streams: IMediaStream[] = [
+    {
+      type: "hls",
+      label: "HLS: Streaming",
+      source:
+        "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8",
+    },
+  ];
+
+  onPlayerReady(api: VgApiService) {
+    this.api = api;
+  }
+
+  ngOnInit() {
+    this.currentStream = this.streams[0];
+  }
+
+  setBitrate(option: BitrateOptions) {
+    if (this.currentStream.type === "hls") {
+      this.vgHls.setBitrate(option);
+    }
+  }
 }
